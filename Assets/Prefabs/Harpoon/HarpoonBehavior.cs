5bf99f2c6bd7828f1c573e0fdc63256d941c646c
Nathan Gromb <nathan.gromb@epfl.ch>
2025-04-13 16:55:22 +0200

    harpoon sounds

--- Assets/Prefabs/Harpoon/HarpoonBehavior.cs
+++ Assets/Prefabs/Harpoon/HarpoonBehavior.cs
@@ -2,9 +2,11 @@ using System;
 using Prefabs.Rope;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.XR;
 using UnityEngine.XR.Interaction.Toolkit;
 using UnityEngine.XR.Interaction.Toolkit.Interactables;
 using UnityEngine.XR.Interaction.Toolkit.Interactors;
+using InputDevice = UnityEngine.XR.InputDevice;
 
 namespace Prefabs.Harpoon
 {
@@ -23,6 +25,12 @@ namespace Prefabs.Harpoon
         
         public String ropeStartTag;
         public String ropeEndTag;
+        
+        public AudioClip startPlacedSound;
+        public AudioClip ropePlacedSound;
+        public AudioClip validTargetSound;
+        public AudioClip invalidTargetSound;
+        public AudioClip itemEquippedSound;
 
         private XRGrabInteractable _grabInteractable;
         private bool _isHeld;
@@ -35,7 +43,9 @@ namespace Prefabs.Harpoon
         private SpriteRenderer _renderer;
         private GameObject _crosshair;
         private Camera _mainCamera;
-        private LineRenderer _lineRenderer;
+        private LineRenderer _ropePreviewLineRenderer;
+        
+        private static readonly Vector3 CrosshairScale = new(0.005f, 0.005f, 0.005f);
 
         private enum Stage
         {
@@ -56,8 +66,9 @@ namespace Prefabs.Harpoon
             _crosshair = new GameObject("CrossSprite");
             _renderer = _crosshair.AddComponent<SpriteRenderer>();
             _renderer.sortingOrder = 10; // Gives priority
-            _crosshair.transform.localScale = new Vector3(0.005f, 0.005f, 0.005f);
+            _crosshair.transform.localScale = CrosshairScale;
             _mainCamera = Camera.main;
+            _ropePreviewLineRenderer = gameObject.AddComponent<LineRenderer>();
         }
 
         protected override void Awake()
@@ -79,24 +90,27 @@ namespace Prefabs.Harpoon
                 _crosshair.transform.LookAt(_mainCamera.transform);
                 _crosshair.transform.Rotate(0, 180, 0);
                 
+                // scale the crosshair to make it visible
+                _crosshair.transform.localScale = CrosshairScale * (1 + Vector3.Distance(_mainCamera.transform.position, hit.point) / 2f);
+                
                 if (_stage == Stage.StartPlaced)
                 {
-                    _lineRenderer ??= _crosshair.AddComponent<LineRenderer>();
-                
-                    _lineRenderer.startWidth = 0.01f;
-                    _lineRenderer.endWidth = 0.01f;
-                    _lineRenderer.positionCount = 2;
-                    _lineRenderer.useWorldSpace = true;
-                    _lineRenderer.material = triggerMaterial;
+                    _ropePreviewLineRenderer.enabled = true;
                     
-                    _lineRenderer.SetPosition(0, _start);
-                    _lineRenderer.SetPosition(1, hit.point);
+                    _ropePreviewLineRenderer.startWidth = 0.01f;
+                    _ropePreviewLineRenderer.endWidth = 0.01f;
+                    _ropePreviewLineRenderer.positionCount = 2;
+                    _ropePreviewLineRenderer.useWorldSpace = true;
+                    _ropePreviewLineRenderer.material = triggerMaterial;
+                    
+                    _ropePreviewLineRenderer.SetPosition(0, _start);
+                    _ropePreviewLineRenderer.SetPosition(1, hit.point);
                 }
             }
             
             if (nextTargetType == TargetType.Valid && _targetType != TargetType.Valid)
             {
-                // todo play sound
+                PlayValidTargetFeedback();
             }
             
             _targetType = nextTargetType;
@@ -164,6 +178,7 @@ namespace Prefabs.Harpoon
         private void OnGrab(SelectEnterEventArgs args)
         {
             _isHeld = true;
+            PlayItemEquippedFeedback();
         }
 
         private void OnRelease(SelectExitEventArgs args)
@@ -179,21 +194,28 @@ namespace Prefabs.Harpoon
 
         private void Shoot()
         {
-            Destroy(_lineRenderer);
-            _lineRenderer = null;
+            if (
+                !Physics.Raycast(harpoonTip.transform.position, harpoonTip.transform.forward, out var hit) 
+                || !IsValidTarget(hit.collider, _stage)
+                )
+            {
+                PlayInvalidTargetFeedback();
+                return;
+            };
             
-            if (!Physics.Raycast(harpoonTip.transform.position, harpoonTip.transform.forward, out var hit)) return;
-            if (!IsValidTarget(hit.collider, _stage)) return;
+            _ropePreviewLineRenderer.enabled = false;
 
             switch (_stage)
             {
                 case Stage.None:
                     _start = hit.point;
                     _stage = Stage.StartPlaced;
+                    PlayStartPlacedFeedback();
                     break;
                 case Stage.StartPlaced:
                     _end = hit.point;
                     _stage = infiniteAmmo ? Stage.None : Stage.Done;
+                    PlayRopePlacedFeedback();
                     PlaceRope();
                     break;
                 case Stage.Done:
@@ -207,6 +229,53 @@ namespace Prefabs.Harpoon
             var rope = Instantiate(ropePrefab);
             rope.GetComponent<RopeBehavior>().ForceUpdate(_start, _end);
         }
-}
 
+        private void PlayStartPlacedFeedback()
+        {
+            PlaySound(startPlacedSound, transform.position, .5f);
+            HapticFeedback(.1f, .5f);
+        }
+        
+        private void PlayRopePlacedFeedback()
+        {
+            PlaySound(ropePlacedSound, transform.position, .5f);
+            HapticFeedback(.5f, .75f);
+        }
+        
+        private void PlayValidTargetFeedback()
+        {
+            PlaySound(validTargetSound, _crosshair.transform.position, .2f);
+            HapticFeedback(.1f, .1f);
+        }
+        
+        private void PlayInvalidTargetFeedback()
+        {
+            PlaySound(invalidTargetSound, transform.position, .1f);
+        }
+        
+        private void PlayItemEquippedFeedback()
+        {
+            PlaySound(itemEquippedSound, transform.position, .5f);
+        }
+
+        private void PlaySound(AudioClip clip, Vector3 position, float volume = 1f)
+        {
+            AudioSource.PlayClipAtPoint(clip, position, volume);
+        }
+
+        private void HapticFeedback(float duration, float amplitude)
+        {
+            InputDevice leftHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.LeftHand);
+            InputDevice rightHandDevice = InputDevices.GetDeviceAtXRNode(XRNode.RightHand);
+            
+            if (leftHandDevice.TryGetHapticCapabilities(out var leftHapticCapabilities) && leftHapticCapabilities.supportsImpulse)
+            {
+                leftHandDevice.SendHapticImpulse(0, amplitude, duration);
+            }
+            if (rightHandDevice.TryGetHapticCapabilities(out var rightHapticCapabilities) && rightHapticCapabilities.supportsImpulse)
+            {
+                rightHandDevice.SendHapticImpulse(0, amplitude, duration);
+            }
+        }
+    }
 }
