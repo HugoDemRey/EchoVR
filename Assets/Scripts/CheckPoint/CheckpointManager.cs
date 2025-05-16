47c79cf104c61d697f5943c34a517e06e832d271
Hugo <hugo.demulerey@gmail.com>
2025-05-16 14:35:26 +0200

    Checkpoints added, remains to add more

--- Assets/Scripts/CheckPoint/CheckpointManager.cs
+++ Assets/Scripts/CheckPoint/CheckpointManager.cs
@@ -4,11 +4,22 @@ using UnityEngine;
 public class CheckpointManager : MonoBehaviour
 {
     private Transform spawnPoint; // The spawn point for the player
-    public void updateSpawnPoint(Transform newSpawnPoint) {
+    public void updateSpawnPoint(Transform newSpawnPoint)
+    {
         spawnPoint = newSpawnPoint; // Update the spawn point to the new one
     }
 
-    public Transform getSpawnPoint() {
-        return spawnPoint; 
+    public Transform getSpawnPoint()
+    {
+        return spawnPoint;
+    }
+    
+    public void teleportPlayer(Transform player)
+    {
+        if (spawnPoint != null)
+        {
+            player.position = spawnPoint.position; // Teleport the player to the spawn point
+            player.rotation = spawnPoint.rotation; // Set the player's rotation to the spawn point's rotation
+        }
     }
 }
